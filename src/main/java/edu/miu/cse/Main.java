971282fb76c1c3c442b05f1eecf7390d00cb94c7
HsuMinHtet <hsumin.hmh@gmail.com>
2024-10-29 12:01:59 -0500

    add second line

--- src/main/java/edu/miu/cse/Main.java
+++ src/main/java/edu/miu/cse/Main.java
@@ -2,5 +2,6 @@ package edu.miu.cse;
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello World");
+        System.out.println("Second line");
     }
 }
